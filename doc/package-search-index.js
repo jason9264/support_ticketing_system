d4ca4c5a54ca8769379f1b6a0c46297ad88a8147
Jason Wang <107277457+jason9264@users.noreply.github.com>
2024-07-03 11:13:12 -0400

    Add files via upload

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"edu.ncsu.csc216.wolf_tickets.model.group"},{"l":"edu.ncsu.csc216.wolf_tickets.model.io"},{"l":"edu.ncsu.csc216.wolf_tickets.model.tickets"},{"l":"edu.ncsu.csc216.wolf_tickets.model.util"},{"l":"edu.ncsu.csc216.wolf_tickets.view.gui"}];updateSearchResults();
